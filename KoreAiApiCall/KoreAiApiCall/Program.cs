17943ee5872a7e568a88e70d0e2a5c7a672f84c7
shivaverma97 <shiva.verma9747@gmail.com>
2022-12-16 12:18:16 +0530

    added api call functions with query and json

---
+++ KoreAiApiCall/KoreAiApiCall/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace KoreAiApiCall
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //add function call before running
+        }
+
+        public void PostApiCallThroughJsonBody()
+        {
+            try
+            {
+                string url = "https://localhost:7230/api/KoreAiCustomerInformation";
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                Encoding encoding = new UTF8Encoding();
+
+                string json = "{\"customerName\":\"test\"," +
+                                  "\"customerEmail\":\"bla\"," +
+                                  "\"customerPhoneNo\":98," +
+                                  "\"customerAge\":21," +
+                                  "\"customerGender\":\"bla\"}";
+
+                byte[] data = encoding.GetBytes(json);
+
+                Stream stream = httpWebRequest.GetRequestStream();
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+
+                Console.WriteLine(json);
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void PostApiCallThroughQueryParams()
+        {
+            try
+            {
+                string url = "https://localhost:7230/api/KoreAiCustomerInformation?customerId=4&emailId=test%40mail.com";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                request.Method = "Post";
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    reader.ReadToEnd();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
